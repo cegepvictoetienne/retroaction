168a7e01bf0913c8d2d854aef711b6fafb48f24f
Etienne Rivard <etienne@kerzo.ca>
2022-05-05 09:54:33 -0400

    Correction saut de page et hauteur des cellules avec longs titres

--- retroaction.py
+++ retroaction.py
@@ -19,6 +19,7 @@ from fpdf import FPDF
 
 # Constantes
 POLICE = r"/Users/etiennerivard/Dropbox/Python/font/DejaVuSans.ttf"
+POLICE_BOLD = r"/Users/etiennerivard/Dropbox/Python/font/DejaVuSans-Bold.ttf"
 
 # Quel est le caractère qui remplace le X pour indiquer que le critère est atteint
 CROCHET = chr(214)
@@ -80,6 +81,7 @@ def traiter_eleve(dossier_sortie, numero_da, feuille_a_traiter, denominateur, co
     pdf = FPDF(orientation="P", unit="in", format="Letter")
     pdf.add_page()
     pdf.add_font('DejaVuSans', fname=POLICE, uni=True)
+    pdf.add_font('DejaVuSansB', fname=POLICE_BOLD, uni=True)
     pdf.set_font('DejaVuSans', size=12)
     pdf.set_fill_color(r=255, g=255, b=255)
 
@@ -116,6 +118,9 @@ def traiter_eleve(dossier_sortie, numero_da, feuille_a_traiter, denominateur, co
 
         pdf.set_font('DejaVuSans', size=12)
 
+        if pdf.will_page_break(HAUTEUR_CELLULE*3):
+            pdf.add_page()
+
         old_position = {
             "x" : pdf.get_x(),
             "y" : pdf.get_y()
@@ -123,15 +128,17 @@ def traiter_eleve(dossier_sortie, numero_da, feuille_a_traiter, denominateur, co
 
         pdf.multi_cell(4, HAUTEUR_CELLULE, titre_critere, bordure, 1, 'L')
 
+        hauteur_valeur = pdf.get_y() - old_position["y"]
+
         # Ajuster la hauteur de la cellule de la valeur pour être identique
         # à la cellule du titre
-        pdf.set_xy(old_position["x"] + 4, pdf.get_y() - HAUTEUR_CELLULE)
+        pdf.set_xy(old_position["x"] + 4, old_position["y"])
 
         if valeur_critere in ("x", "X"):
             valeur_critere = CROCHET
             pdf.set_font(CROCHET_POLICE, '', CROCHET_TAILLE)
 
-        pdf.multi_cell(4, HAUTEUR_CELLULE, valeur_critere, bordure, 1, 'C')
+        pdf.multi_cell(4, hauteur_valeur, valeur_critere, bordure, 1, 'C')
         pdf.ln(0.001)
     # Écrire le PDF sur disque
     try:
